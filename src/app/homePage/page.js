ebb7d8dfeaba0686817bca22b8cade0adc207e46
faheem4531 <aa0561085@gmail.com>
2023-09-25 22:18:09 +0500

    homePage joinUs section

--- src/app/homePage/page.js
+++ src/app/homePage/page.js
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./HomePage.module.css";
 import IntroSection from "./_sections/introSection/IntroSection";
 import AboutUs from "./_sections/aboutUs/AboutUs";
+import Services from "./_sections/services/Services";
+import JoinUs from "./_sections/joinUs/JoinUs";
 import Footer from "../_components/footer/Footer";
 
 const HomePage = () => {
@@ -9,6 +11,8 @@ const HomePage = () => {
     <div className={styles.home}>
       <IntroSection />
       <AboutUs />
+      <Services />
+      <JoinUs />
       <Footer />
     </div>
   )
