8c0b02b0be7f2e38c06a6f09215bcb6a5667618a
faheem4531 <aa0561085@gmail.com>
2023-09-26 15:04:12 +0500

    contactUs responsiveness

--- src/app/connectUs/page.js
+++ src/app/connectUs/page.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./ConnetUs.module.css"
 import GetInTouch from "./getInTouch/GetInTouch";
+import Footer from "../_components/footer/Footer";
 
 const ConnectUs = () => {
   return (
     <div>
       <GetInTouch />
+      <Footer />
     </div>
   )
 };
