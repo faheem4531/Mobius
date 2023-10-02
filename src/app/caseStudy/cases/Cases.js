0cfb925e08918c9fa4935815d011b1a440fe9350
faheem4531 <faheem4531@gmail.com>
2023-10-03 00:09:16 +0500

    case study Page fully responsive

---
+++ src/app/caseStudy/cases/Cases.js
@@ -0,0 +1,93 @@
+import Card from "./Card";
+import styles from "./Cases.module.css"
+import Formula from "@/app/_assets/png/formulaZ.png";
+import Avancus from "@/app/_assets/png/avancus.png";
+import Baraka from "@/app/_assets/png/baraka.png";
+import Elevate from "@/app/_assets/png/elevate.png";
+import Fonestar from "@/app/_assets/png/fonestar.png";
+import Grana from "@/app/_assets/png/grana.png";
+import Houduo from "@/app/_assets/png/houduo.png";
+import Light from "@/app/_assets/png/light-one.png";
+import Luna from "@/app/_assets/png/luna.png";
+import PillPuncher from "@/app/_assets/png/pill-puncher.png";
+import Metaleon from "@/app/_assets/png/metaleon.png";
+import Sony from "@/app/_assets/png/sony.png";
+
+
+const cases_data = [
+  {
+    img: Formula,
+    name: "FORMULA Z",
+    url: "https://www.behance.net/gallery/179175939/Formula-Z/modules/1013175477"
+  },
+  {
+    img: Avancus,
+    name: "AVANCUS",
+    url: "https://www.behance.net/gallery/179276231/Avancus/modules/1018687355"
+  },
+  {
+    img: Sony,
+    name: "SONY",
+    url: "https://www.behance.net/gallery/179199227/Sony/modules/1012195441"
+  },
+  {
+    img: Elevate,
+    name: "ELEVATE",
+    url: "https://www.behance.net/gallery/178830637/Elevate-Beauty-3d-Animation-Fashion/modules/1010063989"
+  },
+  {
+    img: Fonestar,
+    name: "FONESTAR",
+    url: "https://www.behance.net/gallery/179258677/Fonestar/modules/1012553863"
+  },
+  {
+    img: Houduo,
+    name: "HOUDUO",
+    url: "https://www.behance.net/gallery/179280385/Houduo/modules/1012668957"
+  },
+  {
+    img: Luna,
+    name: "LUNA",
+    url: "https://www.behance.net/gallery/178853083/LUNA/modules/1010191853"
+  },
+  {
+    img: Metaleon,
+    name: "METALEON",
+    url: "https://www.behance.net/gallery/179182043/Metaleon/modules/1012103393"
+  },
+  {
+    img: Baraka,
+    name: "BARAKA",
+    url: "https://www.behance.net/gallery/179283511/Baraka/modules/1012685793"
+  },
+  {
+    img: Grana,
+    name: "GRANAGARD",
+    url: "https://www.behance.net/gallery/126319937/GranaGard-Tablets/modules/717030357"
+  },
+  {
+    img: PillPuncher,
+    name: "PILL PUNCHER",
+    url: "https://www.behance.net/gallery/179264673/Pill-Puncher/modules/1018790321"
+  },
+  {
+    img: Light,
+    name: "LIGHT ONE",
+    url: "https://www.behance.net/gallery/178844541/Light-One/modules/1010143713"
+  },
+]
+const Cases = () => {
+  return (
+    <div className={styles.carrers}>
+      {cases_data.map((item) => {
+        return <Card
+          img={item.img}
+          name={item.name}
+          url={item.url}
+        />
+      })}
+    </div>
+  )
+};
+
+export default Cases;
--- src/app/caseStudy/page.js
+++ src/app/caseStudy/page.js
@@ -1,15 +1,17 @@
 import styles from "./CaseStudy.module.css"
 import Footer from "../_components/footer/Footer";
 import IntroSection from "../_components/introSection/IntroSection";
+import Cases from "./cases/Cases";
 
 const CaseStudy = () => {
   return (
     <div className={styles.carrers}>
       <IntroSection
-        heading1="Wanna Join"
-        heading2=" Mobius motion studios?"
-        content="Discover your career path, elevate your prospects. Here are some job vacancies for you, apply now!"
+        heading1="Journey through"
+        heading2="3D Wonders"
+        content="A Showcase of Our Modeling and Animation Case Studies"
       />
+      <Cases />
       <Footer />
     </div>
   )
