08395b6f54d5498639de6880e3ada155ecc466eb
faheem4531 <faheem4531@gmail.com>
2023-10-02 17:46:30 +0500

    carrer page

--- src/app/carrers/page.js
+++ src/app/carrers/page.js
@@ -1,14 +1,14 @@
 import styles from "./Carrers.module.css"
 import Footer from "../_components/footer/Footer";
 import CarrerIntro from "./_sections/introSection/IntroSection";
-import JoinUs from "./_sections/joinUS/JoinJs";
+import JoinUs from "./_sections/joinUS/JoinUs";
 
 const Carrers = () => {
   return (
     <div className={styles.carrers}>
       <CarrerIntro />
       <JoinUs />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   )
 };
