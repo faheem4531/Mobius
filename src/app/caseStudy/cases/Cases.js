12ca30e61dc0e07f424d935086d4513e1070b2a7
faheem4531 <faheem4531@gmail.com>
2023-10-03 15:33:56 +0500

    add key prop in case studies

--- src/app/caseStudy/cases/Cases.js
+++ src/app/caseStudy/cases/Cases.js
@@ -16,64 +16,76 @@ import Sony from "@/app/_assets/png/sony.png";
 
 const cases_data = [
   {
+    id: "c1",
     img: Formula,
     name: "FORMULA Z",
     url: "https://www.behance.net/gallery/179175939/Formula-Z/modules/1013175477"
   },
   {
+    id: "c2",
     img: Avancus,
     name: "AVANCUS",
     url: "https://www.behance.net/gallery/179276231/Avancus/modules/1018687355"
   },
   {
+    id: "c3",
     img: Sony,
     name: "SONY",
     url: "https://www.behance.net/gallery/179199227/Sony/modules/1012195441"
   },
   {
+    id: "c4",
     img: Elevate,
     name: "ELEVATE",
     url: "https://www.behance.net/gallery/178830637/Elevate-Beauty-3d-Animation-Fashion/modules/1010063989"
   },
   {
-    img: Fonestar,
-    name: "FONESTAR",
-    url: "https://www.behance.net/gallery/179258677/Fonestar/modules/1012553863"
-  },
-  {
+    id: "c5",
     img: Houduo,
     name: "HOUDUO",
     url: "https://www.behance.net/gallery/179280385/Houduo/modules/1012668957"
   },
   {
+    id: "c6",
     img: Luna,
     name: "LUNA",
     url: "https://www.behance.net/gallery/178853083/LUNA/modules/1010191853"
   },
   {
+    id: "c7",
     img: Metaleon,
     name: "METALEON",
     url: "https://www.behance.net/gallery/179182043/Metaleon/modules/1012103393"
   },
   {
+    id: "c8",
     img: Baraka,
     name: "BARAKA",
     url: "https://www.behance.net/gallery/179283511/Baraka/modules/1012685793"
   },
   {
+    id: "c9",
     img: Grana,
     name: "GRANAGARD",
     url: "https://www.behance.net/gallery/126319937/GranaGard-Tablets/modules/717030357"
   },
   {
+    id: "c10",
     img: PillPuncher,
     name: "PILL PUNCHER",
     url: "https://www.behance.net/gallery/179264673/Pill-Puncher/modules/1018790321"
   },
   {
+    id: "c11",
     img: Light,
     name: "LIGHT ONE",
     url: "https://www.behance.net/gallery/178844541/Light-One/modules/1010143713"
+  },
+  {
+    id: "c12",
+    img: Fonestar,
+    name: "FONESTAR",
+    url: "https://www.behance.net/gallery/179258677/Fonestar/modules/1012553863"
   },
 ]
 const Cases = () => {
@@ -81,6 +93,7 @@ const Cases = () => {
     <div className={styles.carrers}>
       {cases_data.map((item) => {
         return <Card
+          key={item.id}
           img={item.img}
           name={item.name}
           url={item.url}
