b2d24b9468360fa85b769b2232d8856b6ff92884
faheem4531 <faheem4531@gmail.com>
2023-10-06 22:22:24 +0500

    code refactoring

--- src/app/homePage/_sections/projects/Projects.js
+++ src/app/homePage/_sections/projects/Projects.js
@@ -102,8 +102,8 @@ const Projects = () => {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
+          slidesToScroll: 1,
+          initialSlide: 1
         }
       },
       {
