d416404b1ffdd274f0b11bd1f1d1744225f711fd
faheem4531 <faheem4531@gmail.com>
2024-02-22 16:20:30 +0500

    structuring and fixes

---
+++ src/app/Model/Model.js
@@ -0,0 +1,55 @@
+import { OrbitControls } from '@react-three/drei';
+import { useControls } from 'leva';
+
+const Model = ({ modelStates, models }) => {
+
+  // const { positionT, rotationT, intensity1, intensity2, right } = useControls({
+  //   positionT: { 
+  //     value: { x: -0.7, y: -2, z: 0 },
+  //     step: 0.1,
+  //     joystick: 'invertY'
+  //   },
+  //   intensity1: 2,
+  //   right: {
+  //     value: { x: -0.7, y: -2, z: 0 },
+  //     step: 0.1,
+  //     joystick: 'invertY'
+  //   },
+  //   intensity2: 2,
+  //   rotationT: {
+  //     value: { x: 0.9, y: 4, z: -5.6 },
+  //     step: 0.1,
+  //     joystick: 'invertY'
+  //   },
+  // })
+
+  function setModel() {
+    if (modelStates.model) {
+      return models.lens.scene
+    }
+    else if (modelStates.simulation) {
+      return models.lensWireframe.scene
+    }
+    else if (modelStates.clay) {
+      return models.lensClay.scene
+    }
+  }
+
+  return <>
+    <OrbitControls enableZoom={false} />
+    <directionalLight position={[-8, -0.9, -1.5]} intensity={1} />
+    <directionalLight position={[0.4, -1, -1.1]} intensity={1} />
+    <ambientLight intensity={0.5} />
+
+    {setModel() && (
+      <primitive
+        object={setModel()}
+        scale={0.05}
+        position={[-0.7, -2, 0]}
+        rotation={[0.9, 4, -5.6]}
+      />
+    )}
+  </>
+}
+
+export default Model;
--- src/app/homePage/_sections/services/Services.js
+++ src/app/homePage/_sections/services/Services.js
@@ -3,38 +3,57 @@
 import styles from './Services.module.css'
 import Image from 'next/image';
 import Steps from "@/app/_assets/svg/service-steps.svg";
-import Modeling from "@/app/_assets/svg/3d-service.svg";
+import Step1 from "@/app/_assets/svg/3d-service.svg";
+import Step1Active from "@/app/_assets/svg/3d-service-active.svg";
 import Step2 from "@/app/_assets/svg/simulation-service.svg";
-import Video from "@/app/_assets/svg/video-service.svg";
+import Step2Active from "@/app/_assets/svg/simulation-service-active.svg";
+import Step3 from "@/app/_assets/svg/video-service.svg";
+import Step3Active from "@/app/_assets/svg/video-service-active.svg";
 import Lense from "@/app/_assets/png/lense.png";
 
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
-import { useControls } from 'leva';
+import { useState } from 'react';
+import Model from '@/app/Model/Model';
+import { useGLTF } from '@react-three/drei';
 
 const Services = () => {
 
-  const lens = useGLTF('./lens.glb')
+  const [modelStates, setModelState] = useState({
+    model: true,
+    simulation: false,
+    clay: false
+  })
 
-  // const { positionT, rotationT, intensity1, intensity2, right } = useControls({
-  //   positionT: {
-  //     value: { x: -0.7, y: -2, z: 0 },
-  //     step: 0.1,
-  //     joystick: 'invertY'
-  //   },
-  //   intensity1: 2,
-  //   right: {
-  //     value: { x: -0.7, y: -2, z: 0 },
-  //     step: 0.1,
-  //     joystick: 'invertY'
-  //   },
-  //   intensity2: 2,
-  //   rotationT: {
-  //     value: { x: 0.9, y: 4, z: -5.6 },
-  //     step: 0.1,
-  //     joystick: 'invertY'
-  //   },
-  // })
+  const loadModels = {
+    lens: useGLTF('./lens.glb'),
+    lensClay: useGLTF('./lens-clay.glb'),
+    lensWireframe: useGLTF('./lens-wireframe.glb')
+  };
+
+  function handlingModel() {
+    setModelState({
+      model: true,
+      simulation: false,
+      clay: false
+    })
+  }
+
+  function handlingSimulation() {
+    setModelState({
+      model: false,
+      simulation: true,
+      clay: false
+    })
+  }
+
+  function handlingClay() {
+    setModelState({
+      model: false,
+      simulation: false,
+      clay: true
+    })
+  }
+
 
   return (
     <div className={styles.services}>
@@ -49,27 +68,27 @@ const Services = () => {
             className={styles.steps}
           />
           <div>
-            <h2 className={styles.heading}>
+            <h2 className={`${styles.heading} ${modelStates.model && styles.active}`} onClick={handlingModel}>
               3D Modeling
               <Image
                 alt=''
-                src={Modeling}
+                src={modelStates.model ? Step1Active : Step1}
                 className={styles.logo}
               />
             </h2>
-            <h2 className={`${styles.heading} ${styles.unselected}`}>
+            <h2 className={`${styles.heading} ${styles.unselected} ${modelStates.simulation && styles.active}`} onClick={handlingSimulation}>
               3D Simulation
               <Image
                 alt=''
-                src={Step2}
+                src={modelStates.simulation ? Step2Active : Step2}
                 className={styles.logo}
               />
             </h2>
-            <h2 className={`${styles.heading} ${styles.unselected}`}>
+            <h2 className={`${styles.heading} ${styles.unselected} ${modelStates.clay && styles.active}`} onClick={handlingClay}>
               Video Animation
               <Image
                 alt=''
-                src={Video}
+                src={modelStates.clay ? Step3Active : Step3}
                 className={styles.logo}
               />
             </h2>
@@ -86,20 +105,10 @@ const Services = () => {
             position: [4, 2, 10]
           }}
         >
-          <OrbitControls enableZoom={false} />
-          <directionalLight position={[-8, -0.9, -1.5]} intensity={1} />
-          <directionalLight position={[0.4, -1, -1.1]} intensity={1} />
-          <ambientLight intensity={0.5} />
-          <primitive
-            object={lens.scene}
-            scale={0.05}
-            position={[-0.7, -2, 0]}
-            rotation={[0.9, 4, -5.6]}
-
-          />
+          <Model
+            modelStates={modelStates}
+            models={loadModels} />
         </Canvas>
-
-
 
         <Image
           src={Lense}
