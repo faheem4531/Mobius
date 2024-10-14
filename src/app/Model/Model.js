e95da50d637fdb4665708bb4755a77e26306cadf
faheem4531 <faheem4531@gmail.com>
2024-10-15 03:58:19 +0500

    increase intensity of light for lens model

--- src/app/Model/Model.js
+++ src/app/Model/Model.js
@@ -1,19 +1,17 @@
-'use client'
+"use client";
 
-import { OrbitControls } from '@react-three/drei';
-import { useControls } from 'leva';
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber';
-import { Lens } from '@/app/Model/Lens';
-import { LensClay } from '@/app/Model/LensClay';
-import { LensWireFrame } from './LensWireFrame';
-import { Environment } from '@react-three/drei'
+import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Lens } from "@/app/Model/Lens";
+import { LensClay } from "@/app/Model/LensClay";
+import { LensWireFrame } from "./LensWireFrame";
+import { Environment } from "@react-three/drei";
 
 const Model = ({ modelStates, models }) => {
-
-
   // const { positionT, rotationT, intensity1, intensity2, right } = useControls({
-  //   positionT: { 
+  //   positionT: {
   //     value: { x: -0.7, y: -2, z: 0 },
   //     step: 0.1,
   //     joystick: 'invertY'
@@ -34,40 +32,38 @@ const Model = ({ modelStates, models }) => {
 
   function setModel() {
     if (modelStates.model) {
-      return <Lens />
-    }
-    else if (modelStates.simulation) {
-      return <LensWireFrame />
-    }
-    else if (modelStates.clay) {
-      return <LensClay />
-    }
-    else {
+      return <Lens />;
+    } else if (modelStates.simulation) {
+      return <LensWireFrame />;
+    } else if (modelStates.clay) {
+      return <LensClay />;
+    } else {
       return null;
     }
   }
 
-  return <>
-    <Canvas
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [4, 2, 10]
-      }}
-    >
-      <Suspense fallback={null}>
-        <OrbitControls enableZoom={false} />
-        <directionalLight position={[-8, -0.9, -1.5]} intensity={2} />
-        <directionalLight position={[0.4, -1, -1.1]} intensity={2} />
-        <ambientLight intensity={0.5} />
+  return (
+    <>
+      <Canvas
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [4, 2, 10],
+        }}
+      >
+        <Suspense fallback={null}>
+          <OrbitControls enableZoom={false} />
+          <directionalLight position={[-8, -0.9, -1.5]} intensity={10} />
+          <directionalLight position={[0.4, -1, -1.1]} intensity={10} />
+          <ambientLight intensity={0.5} />
 
-        {/* <Environment preset="sunset" /> */}
-        {setModel()}
-
-      </Suspense>
-    </Canvas>
-  </>
-}
+          {/* <Environment preset="sunset" /> */}
+          {setModel()}
+        </Suspense>
+      </Canvas>
+    </>
+  );
+};
 
 export default Model;
