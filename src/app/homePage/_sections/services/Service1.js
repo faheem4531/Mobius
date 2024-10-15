ad7325815f5eb617070d5a9a5fcf4cd207729a89
faheem4531 <faheem4531@gmail.com>
2024-10-15 06:46:33 +0500

    add services new section

--- src/app/homePage/_sections/services/Service1.js
+++ src/app/homePage/_sections/services/Service1.js
@@ -1,10 +1,157 @@
 import { Box, Typography } from "@mui/material";
 import Image from "next/image";
+import GifImage from "@/app/_assets/png/gif-image.png";
+import Button from "@/app/_components/button/Button";
 
 const Service1 = () => {
   return (
-    <Box>
-      <Typography>my section</Typography>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: { md: "row", sm: "column", xs: "column" },
+        alignItems: "center",
+        background:
+          "linear-gradient(180deg, #151414 0%, rgba(0, 0, 0, 0) 66.61%);",
+        padding: { lg: "100px 70px", sm: "100px 20px", xs: "90px 20px" },
+        columnGap: { lg: "100px", md: "50px" },
+      }}
+    >
+      <Box
+        sx={{
+          maxWidth: { lg: "510px", sm: "450px" },
+          maxHeight: "820px",
+          width: "100%",
+          height: "100%",
+          minWidth: { lg: "500px", md: "350px", sm: "", xs: "" },
+          //   minHeight: { lg: "800px", md: "", sm: "", xs: "" },
+        }}
+      >
+        <Image
+          src={GifImage}
+          alt="gif"
+          style={{ width: "100%", height: "100%" }}
+        />
+      </Box>
+      <Box
+        sx={{
+          maxWidth: { lg: "680px", md: "450px", sm: "450px" },
+          mt: { md: "0", sm: "50px", xs: "50px" },
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: { lg: "20px", sm: "18px", xs: "16px" },
+            fontWeight: 300,
+          }}
+        >
+          Our Services
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: { lg: "38px", md: "32px", xs: "32px" },
+            fontWeight: 800,
+            color: "#FFFFFF",
+            lineHeight: "45.6px",
+            m: "20px 0",
+          }}
+        >
+          Helping Brands Grow Using 3d Product Animation Videos
+        </Typography>
+        <Box sx={{ width: "100px", bgcolor: "#A7A4AD", height: "1px" }}></Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: { lg: "25px", md: "16px", sm: "20px", xs: "15px" },
+            m: "20px 0 30px",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: { lg: "20px", md: "16px", sm: "18px", xs: "16px" },
+              fontWeight: 300,
+            }}
+          >
+            From corporations to start-ups. we create stunning product
+            animations for any product and transform them into living, breathing
+            stories that inspire. Imagine your product not just displayed but
+            showcased with a flair that grabs attention.
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: { lg: "20px", md: "16px", sm: "18px", xs: "16px" },
+              fontWeight: 300,
+            }}
+          >
+            Costly and environment damaging product shoots are a thing of past.
+            Our 3D product animations are crafted with the perfect blend of
+            artistry and technology, making your product the hero of its own
+            narrative.
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: { lg: "20px", md: "16px", sm: "18px", xs: "16px" },
+              fontWeight: 700,
+            }}
+          >
+            The benefits are immense
+          </Typography>
+          <Box
+            sx={{
+              fontSize: { lg: "20px", md: "16px", sm: "18px", xs: "16px" },
+              fontWeight: 300,
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: { lg: "20px", md: "16px", sm: "18px", xs: "16px" },
+                fontWeight: 700,
+                display: "inline-block",
+              }}
+            >
+              - Increased Engagement:
+            </Typography>{" "}
+            Drive and immerse more audience in dynamic content
+          </Box>
+          <Box
+            sx={{
+              fontSize: { lg: "20px", sm: "18px", xs: "16px" },
+              fontWeight: 300,
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: { lg: "20px", sm: "18px", xs: "16px" },
+                fontWeight: 700,
+                display: "inline-block",
+              }}
+            >
+              - Intricate Details:
+            </Typography>{" "}
+            Display intricate details and processes with dazzling details
+          </Box>
+          <Box
+            sx={{
+              fontSize: { lg: "20px", sm: "18px", xs: "16px" },
+              fontWeight: 300,
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: { lg: "20px", sm: "18px", xs: "16px" },
+                fontWeight: 700,
+                display: "inline-block",
+              }}
+            >
+              - Foster Sales:
+            </Typography>{" "}
+            Carry viewers to a experienceable journey, fostering informed buying
+            decisions
+          </Box>
+        </Box>
+        <Button text={"Get in touch now!"} />
+      </Box>
     </Box>
   );
 };
