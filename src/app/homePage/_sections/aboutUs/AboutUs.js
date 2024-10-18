a233933683a44794df521f924a4ac11b849117df
faheem4531 <faheem4531@gmail.com>
2024-10-19 02:45:43 +0500

    about us animation

--- src/app/homePage/_sections/aboutUs/AboutUs.js
+++ src/app/homePage/_sections/aboutUs/AboutUs.js
@@ -10,12 +10,12 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 const AboutUs = () => {
   const textRef = useRef(null);
   const imageRef = useRef(null);
+  const headingRef = useRef(null);
+  const paragraphRef = useRef(null);
 
   useEffect(() => {
-    // Register the ScrollTrigger plugin
     gsap.registerPlugin(ScrollTrigger);
 
-    // Animation for text flying in from left to right
     gsap.fromTo(
       textRef.current,
       { x: -200, opacity: 0 },
@@ -26,14 +26,47 @@ const AboutUs = () => {
         ease: "power2.out",
         scrollTrigger: {
           trigger: textRef.current,
-          start: "top 80%", // Start animation when the top of the element is 80% from the top of the viewport
+          start: "top 80%",
           end: "top 30%",
           toggleActions: "play none none reverse",
         },
       }
     );
 
-    // Animation for image flying in from bottom to top
+    gsap.fromTo(
+      headingRef.current,
+      { x: -350, opacity: 0 },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1.2,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: headingRef.current,
+          start: "top 80%",
+          end: "top 30%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+
+    gsap.fromTo(
+      paragraphRef.current,
+      { x: -500, opacity: 0 },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1.4,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: paragraphRef.current,
+          start: "top 80%",
+          end: "top 30%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+
     gsap.fromTo(
       imageRef.current,
       { y: 200, opacity: 0 },
@@ -59,15 +92,17 @@ const AboutUs = () => {
       </div>
       <div
         className={styles.content}
-        ref={textRef}
         style={{ position: "relative", zIndex: 2 }}
       >
-        <h4>About Us</h4>
-        <h2 style={{ color: "var(--text-main)", fontSize: "38px" }}>
+        <h4 ref={textRef}>About Us</h4>
+        <h2
+          ref={headingRef}
+          style={{ color: "var(--text-main)", fontSize: "38px" }}
+        >
           Impracticality To Reality
         </h2>
         <hr />
-        <p>
+        <p ref={paragraphRef}>
           At Mobius Studios, 3D animation is not just a job for us. It&apos;s
           our true passion. We&apos;re a team of passionate project managers,
           creative directors, 3D animators, CGI artists, and Simulation experts
