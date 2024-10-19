5700fec782c9d864be636d72c2ef3fb86c5faf0b
faheem4531 <faheem4531@gmail.com>
2024-10-20 04:13:39 +0500

    add microsoft-clarity

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,20 @@
 
 import Script from "next/script";
 import HomePage from "./homePage/page";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "XXXXXXX");
+        `}
+      </Script>
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXX"
