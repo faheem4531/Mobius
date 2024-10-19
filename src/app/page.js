3cce0bbfa27b58e51b7d261df61015c612c1e221
faheem4531 <faheem4531@gmail.com>
2024-10-20 03:52:28 +0500

    add googla analytics script

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,26 @@
-'use client'
+"use client";
 
-import styles from './page.module.css'
-import HomePage from './homePage/page'
+import Script from "next/script";
+import HomePage from "./homePage/page";
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXX"
+      />
+
+      <Script id="google-analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-RGM8D41H5K');
+          `}
+      </Script>
       <HomePage />
-    </div>
-  )
-};
+    </>
+  );
+}
