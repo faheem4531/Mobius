7b99b9b79ea53293d1a7f71c3011ac178c6ce7e9
faheem4531 <faheem4531@gmail.com>
2024-10-22 19:58:34 +0500

    Tawk.to Live Chat

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
           (function() {
               var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
               s1.async = true;
-              s1.src = 'https://embed.tawk.to/XXXXXX/default'; 
+             s1.src='https://embed.tawk.to/671632c44304e3196ad5088a/1ianbckfj';
               s1.charset = 'UTF-8';
               s1.setAttribute('crossorigin', '*');
               s0.parentNode.insertBefore(s1, s0);
