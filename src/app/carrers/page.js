52ba6402b7f8bb7b395d9d7f09d7a7b3a61a1c7b
faheem4531 <faheem4531@gmail.com>
2024-10-29 00:24:11 +0500

    meta data for carrers

--- src/app/carrers/page.js
+++ src/app/carrers/page.js
@@ -1,26 +1,34 @@
-'use client'
+"use client";
 
-import styles from "./Carrers.module.css"
+import styles from "./Carrers.module.css";
 import Footer from "../_components/footer/Footer";
 import IntroSection from "../_components/introSection/IntroSection";
 import JoinUs from "./_sections/joinUS/JoinUs";
-import Head from 'next/head';
+import Head from "next/head";
 
 const Carrers = () => {
   return (
-    <div className={styles.carrers}>
+    <>
       <Head>
+        <title>Careers</title>
+        <meta
+          name="description"
+          content="Join Mobius Studios and be part of a creative team shaping the future of CGI and animation. Explore exciting career opportunities with us today!"
+        />
         <link rel="canonical" href="https://www.mobiusstudios.co/carrers" />
       </Head>
-      <IntroSection
-        heading1="Wanna Join"
-        heading2=" Mobius motion studios?"
-        content="Discover your career path, elevate your prospects. Here are some job vacancies for you, apply now!"
-      />
-      <JoinUs />
-      <Footer />
-    </div>
-  )
+
+      <div className={styles.carrers}>
+        <IntroSection
+          heading1="Wanna Join"
+          heading2=" Mobius motion studios?"
+          content="Discover your career path, elevate your prospects. Here are some job vacancies for you, apply now!"
+        />
+        <JoinUs />
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default Carrers;
