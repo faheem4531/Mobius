f8be7b532829619cc364b9d79d4a1c96e259e6db
faheem4531 <faheem4531@gmail.com>
2024-10-29 00:28:33 +0500

    delete useless files

--- src/app/carrers/page.js
+++ src/app/carrers/page.js
@@ -1,10 +1,10 @@
 "use client";
 
-import styles from "./Carrers.module.css";
+import Head from "next/head";
+
 import Footer from "../_components/footer/Footer";
 import IntroSection from "../_components/introSection/IntroSection";
 import JoinUs from "./_sections/joinUS/JoinUs";
-import Head from "next/head";
 
 const Carrers = () => {
   return (
@@ -18,10 +18,10 @@ const Carrers = () => {
         <link rel="canonical" href="https://www.mobiusstudios.co/carrers" />
       </Head>
 
-      <div className={styles.carrers}>
+      <div>
         <IntroSection
           heading1="Wanna Join"
-          heading2=" Mobius motion studios?"
+          heading2="Mobius motion studios?"
           content="Discover your career path, elevate your prospects. Here are some job vacancies for you, apply now!"
         />
         <JoinUs />
