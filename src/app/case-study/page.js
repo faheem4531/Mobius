3b045b97ad809e18a5d33f77c891e352ec89fc80
faheem4531 <faheem4531@gmail.com>
2024-10-29 00:25:55 +0500

    code refactoring

--- src/app/case-study/page.js
+++ src/app/case-study/page.js
@@ -2,7 +2,6 @@
 
 import Head from "next/head";
 
-import styles from "./CaseStudy.module.css";
 import Footer from "../_components/footer/Footer";
 import IntroSection from "../_components/introSection/IntroSection";
 import Cases from "./cases/Cases";
@@ -18,7 +17,8 @@ const CaseStudy = () => {
         />
         <link rel="canonical" href="https://www.mobiusstudios.co/case-study" />
       </Head>
-      <div className={styles.carrers}>
+
+      <div>
         <IntroSection
           heading1="Journey through"
           heading2="3D Wonders"
