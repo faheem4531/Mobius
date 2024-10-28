eb6336570195ad2451facb9c860fcc3ae6fd7be1
faheem4531 <faheem4531@gmail.com>
2024-10-29 00:24:21 +0500

    meta data for case study

--- src/app/case-study/page.js
+++ src/app/case-study/page.js
@@ -1,25 +1,34 @@
-'use client'
+"use client";
 
-import styles from "./CaseStudy.module.css"
+import Head from "next/head";
+
+import styles from "./CaseStudy.module.css";
 import Footer from "../_components/footer/Footer";
 import IntroSection from "../_components/introSection/IntroSection";
 import Cases from "./cases/Cases";
-import Head from 'next/head';
+
 const CaseStudy = () => {
   return (
-    <div className={styles.carrers}>
+    <>
       <Head>
-      <link rel="canonical" href="https://www.mobiusstudios.co/case-study" />
+        <title>Case Studies</title>
+        <meta
+          name="description"
+          content="Explore Mobius Studios' past projects and see how our CGI and animation solutions drive visual excellence and success for businesses of all sizes."
+        />
+        <link rel="canonical" href="https://www.mobiusstudios.co/case-study" />
       </Head>
-      <IntroSection
-        heading1="Journey through"
-        heading2="3D Wonders"
-        content="A Showcase of Our Modeling and Animation Case Studies"
-      />
-      <Cases />
-      <Footer />
-    </div>
-  )
+      <div className={styles.carrers}>
+        <IntroSection
+          heading1="Journey through"
+          heading2="3D Wonders"
+          content="A Showcase of Our Modeling and Animation Case Studies"
+        />
+        <Cases />
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default CaseStudy;
