1786c11d9956d2d8c27551d6361d14b9b595072a
faheem4531 <faheem4531@gmail.com>
2024-10-29 00:18:01 +0500

    home title and discription

--- src/app/homePage/page.js
+++ src/app/homePage/page.js
@@ -11,24 +11,34 @@ import Service2 from "./_sections/services/Service2";
 import Service3 from "./_sections/services/Service3";
 import OurProcess from "./_sections/ourProcess/OurProcess";
 import GotQuestions from "./_sections/gotQuestions/GotQuestions";
+import Head from "next/head";
 
 const HomePage = () => {
   return (
-    <div>
-      <IntroSection />
-      <AboutUs />
-      <Services />
-      <Service1 />
-      <Service2 />
-      <Service3 />
-      <Projects />
-      <Discover />
-      <OurProcess />
-      <Clients />
-      <GotQuestions />
-      <JoinUs />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Mobius Studios | 3d animation & CGI videos in USA</title>
+        <meta
+          name="description"
+          content="Mobius Studios, a USA-based 3D animation & CGI video agency, creates stunning animations to enhance your brand’s social media, ads, and digital marketing presence"
+        />
+      </Head>
+      <div>
+        <IntroSection />
+        <AboutUs />
+        <Services />
+        <Service1 />
+        <Service2 />
+        <Service3 />
+        <Projects />
+        <Discover />
+        <OurProcess />
+        <Clients />
+        <GotQuestions />
+        <JoinUs />
+        <Footer />
+      </div>
+    </>
   );
 };
 
