e3ade0df5e0a84e47b11a3a1278d7d5f8f3c5c3b
faheem4531 <faheem4531@gmail.com>
2025-10-23 23:48:22 +0500

    divider setings

--- src/_components/design-home/dividers.js
+++ src/_components/design-home/dividers.js
@@ -1,15 +1,14 @@
-"use client";
-
 import { Box } from "@mui/material";
 
 const Dividers = () => {
   return (
     <Box
       sx={{
-        m: "auto",
-        maxWidth: "200px",
+        m: "5px auto",
+        maxWidth: { sm: "200px", xs: "90px" },
         width: "100%",
-        height: "1px",
+        height: { sm: "0.5px", xs: "0.1px" },
+        maxHeight: { sm: "0.5px", xs: "0.1px" },
         bgcolor: "#D9D9D9",
       }}
     />
