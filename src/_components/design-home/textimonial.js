0db8f8fa1bd01fbc01b1611d58c522f4b6970dbe
faheem4531 <faheem4531@gmail.com>
2025-11-12 18:18:30 +0500

    reviews section update

--- src/_components/design-home/textimonial.js
+++ src/_components/design-home/textimonial.js
@@ -1,35 +1,48 @@
 import { Box, Typography } from "@mui/material";
 import Image from "next/image";
-import ImageReview from "@/_assets/png/ImageReview.png";
 
-const ReviewTestmonial = ({ name }) => {
+const ReviewTestimonial = ({
+  name,
+  role,
+  company,
+  review,
+  rating = 5,
+  imageSrc,
+  bgImage = "/clients-bg.webp",
+}) => {
   return (
     <Box
       sx={{
-        backgroundImage: "url('/clients-bg.webp')",
+        backgroundImage: `url(${bgImage})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        p: { xs: 2, md: 4 },
       }}
     >
       <Box
         sx={{
-          // maxWidth: { xs: "400px" },
           width: { xs: "300px", md: "400px" },
-          padding: { xs: "16px", md: "24px" },
+          p: { xs: 2, md: 3 },
           textAlign: "start",
           borderRadius: "24px",
           border: "var(--border-lite-3)",
           background: "linear-gradient(var(--bg-gradient-1))",
         }}
       >
-        <Box sx={{ display: "flex", gap: "8px" }}>
+        <Box sx={{ display: "flex", gap: "8px", alignItems: "center" }}>
           <Box sx={{ width: { xs: "48px", md: "64px" }, aspectRatio: "1/1" }}>
             <Image
-              src={ImageReview}
-              alt="ImageReview"
+              src={imageSrc}
+              alt={`${name}-review`}
+              width={64}
+              height={64}
               style={{
                 width: "100%",
+                borderRadius: "12px",
                 height: "100%",
                 objectFit: "contain",
               }}
@@ -50,25 +63,22 @@ const ReviewTestmonial = ({ name }) => {
               {name}
             </Typography>
 
-            <div>
-              {/* ⭐ SVG stars */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="98"
-                height="19"
-                viewBox="0 0 98 19"
-                fill="none"
-              >
-                {[...Array(5)].map((_, i) => (
-                  <path
-                    key={i}
-                    d="M10.0186 0.078125L12.8324 5.50308L19.2914 6.29697L14.5715 10.4437L15.7495 16.3593L10.0186 13.4971L4.28765 16.3593L5.46561 10.4437L0.745753 6.29697L7.20468 5.50308L10.0186 0.078125Z"
-                    transform={`translate(${i * 19}, 0)`}
-                    fill="#BC9362"
-                  />
-                ))}
-              </svg>
-            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="98"
+              height="19"
+              viewBox="0 0 98 19"
+              fill="none"
+            >
+              {[...Array(rating)].map((_, i) => (
+                <path
+                  key={i}
+                  d="M10.0186 0.078125L12.8324 5.50308L19.2914 6.29697L14.5715 10.4437L15.7495 16.3593L10.0186 13.4971L4.28765 16.3593L5.46561 10.4437L0.745753 6.29697L7.20468 5.50308L10.0186 0.078125Z"
+                  transform={`translate(${i * 19}, 0)`}
+                  fill="#BC9362"
+                />
+              ))}
+            </svg>
           </Box>
         </Box>
 
@@ -76,16 +86,13 @@ const ReviewTestmonial = ({ name }) => {
           variant="body1"
           sx={{
             fontSize: { xs: "12px", md: "16px" },
-            padding: "32px 0",
+            py: 4,
             color: "#EDEDEF",
             lineHeight: "120%",
             fontFamily: "Avenir3 !important",
           }}
         >
-          “Regardless of the type of testimonial you like to use, letting people
-          know exactly what benefits your product or service can offer them in
-          the words of people. Product or service can offer them in the words of
-          people.”
+          “{review}”
         </Typography>
 
         <Typography
@@ -96,7 +103,7 @@ const ReviewTestmonial = ({ name }) => {
             textTransform: "capitalize",
           }}
         >
-          coordinator at -{" "}
+          {role} at -{" "}
           <Typography
             component="span"
             sx={{
@@ -107,7 +114,7 @@ const ReviewTestmonial = ({ name }) => {
               fontFamily: "Avenir !important",
             }}
           >
-            Luna
+            {company}
           </Typography>
         </Typography>
       </Box>
@@ -115,4 +122,4 @@ const ReviewTestmonial = ({ name }) => {
   );
 };
 
-export default ReviewTestmonial;
+export default ReviewTestimonial;
